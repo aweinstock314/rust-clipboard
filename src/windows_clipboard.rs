6cdd27d5bc2325738ea056793155624ea9b91f29
Douman <douman@gmx.se>
2015-09-20 10:47:39 +0300

    Win: set_contents() accepts AsRef<OsStr>

--- src/windows_clipboard.rs
+++ src/windows_clipboard.rs
@@ -1,17 +1,23 @@
 use clipboard_win::{get_clipboard_string, set_clipboard};
 
 use std::error::Error;
+use std::ffi::OsStr;
 
 pub struct ClipboardContext;
 
 impl ClipboardContext {
+    #[inline]
     pub fn new() -> Result<ClipboardContext, Box<Error>> {
         Ok(ClipboardContext)
     }
+
+    #[inline]
     pub fn get_contents(&self) -> Result<String, Box<Error>> {
         Ok(try!(get_clipboard_string()))
     }
-    pub fn set_contents(&mut self, data: String) -> Result<(), Box<Error>> {
-        Ok(try!(set_clipboard(&data)))
+
+    #[inline]
+    pub fn set_contents<T: AsRef<OsStr>>(&mut self, data: T) -> Result<(), Box<Error>> {
+        Ok(try!(set_clipboard(data.as_ref())))
     }
 }
